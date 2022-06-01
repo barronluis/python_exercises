56fbb86afa8b481bce41fbd18285364dd12f23ba
barronluis <luis.barron@enroutesystems.com>
2022-06-01 13:46:15 -0500

    All exercises resolved (Comments deleted)

--- palindrome_words.py
+++ palindrome_words.py
@@ -6,8 +6,5 @@ def palindrome(word: str = 'ana'):
         return print("Is not palindrome")
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     palindrome('anitalavalatina')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
